f96135bcf95eddef87e1f3b36d4bcb239e965367
Yashprit Singh <yashprits@gmail.com>
2015-11-29 22:09:20 +0530

    added dynamic algorithm

---
+++ src/dynamic-algorithm/max-triangle-sum.js
@@ -0,0 +1,26 @@
+module.exports = function (triangle) {
+  while (triangle.length !== 1) {
+    var length = triangle.length;
+    var row = [];
+
+    var currentArray = length - 2;
+
+    var endArray = length - 1;
+
+    var currentArray = triangle[currentArray];
+
+    for (var i = 0; i < currentArray.length; i++) {
+      var result = currentArray[i] + Math.max(triangle[endArray][i], triangle[endArray][i + 1]);
+      row.push(result);
+    }
+
+    triangle.pop();
+    triangle.pop();
+
+    triangle.push(row);
+  }
+  
+  var result = triangle[0][0]
+
+  return result;
+}
